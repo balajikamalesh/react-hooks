8260c3008eb3caa9b8b38b20bb315b39385974cb
Balaji <abalajikamalesh@gmail.com>
2021-03-31 00:08:58 +0530

    Add useState and useEffect

---
+++ src/hooks/useEffect.js
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from "react";
+
+export default () => {
+  const [time, setTime] = useState(new Date());
+
+  useEffect(() => {
+    const timer = setTimeout(() => setTime(new Date()), 1000);
+    return () => clearTimeout(timer);
+  });
+
+  return <h1>useEffect Example: {time.toLocaleTimeString()}</h1>;
+};
---
+++ src/hooks/useState.js
@@ -0,0 +1,14 @@
+import React, { useState } from "react";
+
+export default () => {
+  const [isGreen, setIsGreen] = useState(true);
+
+  return (
+    <h1
+      onClick={() => setIsGreen(!isGreen)}
+      style={{ color: isGreen ? "limegreen" : "crimson" }}
+    >
+      useState Example
+    </h1>
+  );
+};
